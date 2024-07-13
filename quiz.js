44606ece85d8c11b2a9fd30c46b635fff424a466
said-ops <idrissisaid29@gmail.com>
2024-07-13 23:05:42 +0000

    quiz emplementation

---
+++ quiz.js
@@ -0,0 +1,16 @@
+function checkAnswer(){
+    const correctAnswer = "4";
+    let userAnswer = null;
+    let feedback = document.getElementById("feedback");
+    let inputs =document.querySelectorAll("input[name='quiz']");
+
+    inputs.forEach(element => {
+        if(element.checked===true)
+        userAnswer=element.value;
+    });
+   if (userAnswer===correctAnswer)
+    feedback.textContent="Correct! Well done";
+   else
+   feedback.textContent="That's incorrect. Try again!"
+}
+document.getElementById('submit-answer').addEventListener('click',checkAnswer);

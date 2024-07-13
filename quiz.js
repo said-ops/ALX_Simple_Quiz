90b3cfd2726ec426f1212444f072b3cf850bcb87
said-ops <idrissisaid29@gmail.com>
2024-07-13 23:11:43 +0000

    quiz emplementation

--- quiz.js
+++ quiz.js
@@ -9,7 +9,7 @@ function checkAnswer(){
         userAnswer=element.value;
     });
    if (userAnswer === correctAnswer)
-    feedback.textContent = "Correct! Well done";
+    feedback.textContent = "Correct! Well done.";
    else
    feedback.textContent = "That's incorrect. Try again!"
 }
